1d3e60fb0a39d47ed66ace974d40f02eb498979f
Bella28sli <izabellaslivina@gmail.com>
2026-05-15 02:10:01 +0300

    Restore InkMatch everything

--- InkMatchWeb/dashboard/admin.py
+++ InkMatchWeb/dashboard/admin.py
@@ -4,6 +4,7 @@ from django.contrib.auth.models import Group, User
 admin.site.site_header = 'InkMatch Admin'
 admin.site.site_title = 'InkMatch'
 admin.site.index_title = 'Панель управления InkMatch'
+admin.site.site_url = ''
 
 admin.site.unregister(Group)
 admin.site.unregister(User)
@@ -12,5 +13,4 @@ admin.site.unregister(User)
 @admin.register(User)
 class InkMatchUserAdmin(admin.ModelAdmin):
     list_display = ('username', 'email', 'is_staff', 'is_superuser', 'is_active')
-    search_fields = ('username', 'email')
     list_filter = ('is_staff', 'is_superuser', 'is_active')
--- InkMatchWeb/inkmatchweb/urls.py
+++ InkMatchWeb/inkmatchweb/urls.py
@@ -1,7 +1,10 @@
 from django.contrib import admin
 from django.urls import include, path
 
+from dashboard import views as dashboard_views
+
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('dashboard.urls')),
+    path('', dashboard_views.landing, name='site-home'),
+    path('panel/', include('dashboard.urls')),
 ]
